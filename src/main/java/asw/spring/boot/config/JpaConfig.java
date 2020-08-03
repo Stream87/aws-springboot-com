521353b9f823b20100b426130d94b8deadd45135
stream87 <diego87@naver.com>
2020-08-04 04:14:36 +0900

    init

---
+++ src/main/java/asw/spring/boot/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package asw.spring.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA Auditing 활성화
+public class JpaConfig {}
